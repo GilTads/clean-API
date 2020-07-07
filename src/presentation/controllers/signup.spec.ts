cdf903f5eba71953fdb7a8762a95cf6b76aa416b
gilTads <gil.tads@gmail.com>
2020-07-06 22:36:55 -0400

    test: ensure SignupController returns 400 if an invalid email is provided (error)

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,10 +1,18 @@
 import { SignupController } from './signup'
 import { MissingPramError } from '../errors/missing-param-error'
+import { InvalidPramError } from '../errors/invalid-param-error'
+import { EmailValidator } from '../protocols/email-validator'
 
 // sut = System Under Test
 
 const makeSut = (): SignupController => {
-  return new SignupController()
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  const emailValidatorStub = new EmailValidatorStub()
+  return new SignupController(emailValidatorStub)
 }
 
 describe('Signup Controller', () => {
@@ -56,11 +64,26 @@ describe('Signup Controller', () => {
       body: {
         name: 'any_name',
         email: 'any_email@email.com',
-        password: 'any_password'
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
       }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingPramError('passwordConfirmation'))
+  })
+  test('should return 400 if an ivalid email is provided', () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidPramError('email'))
   })
 })
