f51bb5aa30a3b164f8a5bcb0de46750ec49f63c2
gilTads <gil.tads@gmail.com>
2020-07-16 22:55:49 -0400

    refactor: move serverError to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,9 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingPramError } from '../errors/missing-param-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { InvalidPramError } from '../errors/invalid-param-error'
-import { ServerError } from '../errors/server-error'
 
 export class SignupController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -18,15 +17,15 @@ export class SignupController implements Controller {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
-          return badRequest(new MissingPramError(field), 400)
+          return badRequest(new MissingPramError(field))
         }
         const isValid = this.emailValidator.isValid(httpRequest.body.email)
         if (!isValid) {
-          return badRequest(new InvalidPramError('email'), 400)
+          return badRequest(new InvalidPramError('email'))
         }
       }
     } catch (error) {
-      return badRequest(new ServerError(), 500)
+      return serverError()
     }
   }
 }
