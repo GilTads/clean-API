874befc80779469bf76fc5f054c3af45b15dfb21
GilTads <gil.tads@gmail.com>
2020-08-07 11:05:29 -0400

    test: ensure return true if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -16,7 +16,7 @@ describe('EmailValidator Adapter', () => {
   })
   it('should return true if validator returns true', () => {
     const sut = new EmailValidatorAdapter()
-    const isValid = sut.isValid('invalid_email@email.com')
+    const isValid = sut.isValid('valid_email@email.com')
     expect(isValid).toBe(true)
   })
 })
