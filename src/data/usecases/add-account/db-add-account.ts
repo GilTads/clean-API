c78cc841965d0e7c0c041f386486036e4480859c
GilTads <gil.tads@gmail.com>
2020-08-10 22:19:16 -0400

    refactor: add await in add method

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -6,7 +6,7 @@ export class DbAddAccount implements AddAccount {
   constructor (private readonly encrypter: Encrypter) {}
 
   async add (account: AddAccountModel): Promise<AccountModel> {
-    this.encrypter.encrypt(account.password)
+    await this.encrypter.encrypt(account.password)
     return new Promise(resolve => resolve(undefined))
   }
 }
